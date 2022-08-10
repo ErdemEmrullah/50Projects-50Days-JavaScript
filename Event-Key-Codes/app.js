f1024ac147ceb48eea6d4995e34cc4d5d62f1c76
Emrullah Erdem <emrullah.erdem@outlook.com>
2022-08-10 09:48:29 +0300

    keycode searcher

---
+++ Event-Key-Codes/app.js
@@ -0,0 +1,20 @@
+const insert = document.getElementById("insert")
+window.addEventListener("keydown",(e) =>{
+    keyCode(e)
+})
+
+function keyCode(){
+    insert.innerHTML= `
+    <div class="key">
+    ${event.key === '' ? ' Space' : event.key}
+<small>event.key</small>
+</div>
+<div class="key">
+    ${event.keyCode}
+    <small>event.keyCode</small>
+</div>
+<div class="key">
+    ${event.code}
+    <small>event.code</small>
+</div>`
+}
