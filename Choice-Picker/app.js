69c328f5652e68ee9479a58026711e107ee9a4be
Emrullah Erdem <emrullah.erdem@outlook.com>
2022-08-10 14:45:26 +0300

    random selector and create span class

---
+++ Choice-Picker/app.js
@@ -0,0 +1,37 @@
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
+
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+  createTags(e.target.value)
+})
+
+function createTags(input) {
+  const tags = input
+    .split(',')
+    .filter((tag) => tag.trim() !== '')
+    .map((tag) => tag.trim())
+
+  tagsEl.innerHTML = ''
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement('span')
+    tagEl.classList.add('tag')
+    tagEl.innerText=tag
+    tagsEl.appendChild(tagEl)
+  })
+}
+
+function randomSelect(){
+    const times =30
+
+    const interval =setInterval(()=>{
+        const randomTag= picckRandomTag()
+    },100)
+}
+
+function picckRandomTag(){
+    const tags = document.querySelectorAll('tag')
+    
+}
